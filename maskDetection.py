3b8e11ff32cf68e4dad5fa3184fd994108c4867d
Conley <71302026+conley21p@users.noreply.github.com>
2020-11-06 13:28:28 -0600

    Update maskDetection.py
    Cleaned up, added comments

--- maskDetection.py
+++ maskDetection.py
@@ -8,24 +8,16 @@ import tensorflow.keras as keras
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 from tensorflow.keras.optimizers import Adam
-#from tensorflow.keras.layers import Conv2D as Conv2d
-#from tensorflow import nn as n
 
-#TODO: Format Data #DEFINE input_shape
-
-
+# The Data set being used https://github.com/Kbfrancq/AI_Club_20-21/tree/main/small_data
 PATH =  "C:/Users/conle/Documents/AiClub/AI_Club_20-21/small_data"
 CATEG = ["with", "without"]
 
-
-
-#Define shape 50 x50
+#Define shape images will be rendered to
 SIZE = 100
-
-
-#Normalize data
-
+#Format images 
 training_data = []
+
 #create training data
 def createTD():
     #iterate through all examples
@@ -40,7 +32,11 @@ def createTD():
                 training_data.append([new_array, class_num])
             except Exception as e:
                 pass
-      #normalize method      
+"""
+This method changes the values within the training data 
+within between 0-1.
+This is important so the number don't get to big when fitting the model
+"""
 def norm(tester):
     new= []
     for i in range(len(tester)):
@@ -54,35 +50,39 @@ def norm(tester):
     return new
 
 createTD()
+
+#Lines 55-58 make a test set from taking data out of the training data
 mid = (len(training_data)/2)
 test= []
 for i in range(5,-5,-1):
     test.append(training_data.pop(int(mid+i)))
 
+    
 print(len(training_data))
 print(len(test))
 
-#shuffle the list of data
+#shuffle the list of training data
 random.shuffle(training_data)
 
 
+"""Seperate the trainging data into 
+(x) the images and (y) the key (mask/noMask)"""
 x=[]
 y=[]
-
 for features,label in training_data: 
     x.append(features)#the actaul info in the data
     y.append(label) # 1(without mask) or 0 (with mask)
-    
 xTest = []
 yTest = []
 for feature,label in test:
     xTest.append(features)
     yTest.append(label)
-#noramlize data between 0-1
-x = norm(x)
-xTest = norm(xTest)
-input_shape = [SIZE,SIZE,1]
-#// Define Train`/Test Sets
+    
+x = norm(x)    #noramlize data between 0-1
+xTest = norm(xTest)   #noramlize data between 0-1
+input_shape = [SIZE,SIZE,1]   #define the input shape [img_width,img_height,valueRange]
+
+
 #// build model
 print("building model")
 model = keras.Sequential(); 
@@ -92,6 +92,7 @@ model.add(keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(128, activation='relu'))
 model.add(keras.layers.Dense(1, activation='sigmoid'))
+
 print("compiling model")
 # compile model
 opt = Adam(lr=.0001, decay=.0004 / 29)
@@ -99,8 +100,8 @@ model.compile(loss="mse",
              optimizer=opt,
              metrics=['accuracy'])
              
-print("fitting model")       
-#// run data
+#Fit the model
+print("fitting model") 
 x = np.array(x).reshape(len(x),SIZE,SIZE,1).tolist()
 H = model.fit(x,y,batch_size=128, epochs=5)
 
@@ -109,8 +110,3 @@ print("evaluting")
 loss, acc = model.evaluate(xTest,yTest)  # evaluate the out of sample data with model
 print(loss)  # model's loss (error)
 print(acc)  # model's accuracy
-
-
-
-
-
