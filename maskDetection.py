b5444f95f31c4cc33c8af6874e1f30506b080bdf
Conley <conley21p@gmail.com>
2020-11-12 08:19:24 -0600

    modified maskDetection

--- maskDetection.py
+++ maskDetection.py
@@ -1,24 +1,21 @@
 import numpy as np
-#import matplotlib.pyplot as plt
 import random
 import os
 import cv2
-#import pickle
 import tensorflow.keras as keras
-from sklearn.metrics import classification_report
-import matplotlib.pyplot as plt
 from tensorflow.keras.optimizers import Adam
 
-# The Data set being used https://github.com/Kbfrancq/AI_Club_20-21/tree/main/small_data
+#data set paths
 PATH =  "C:/Users/conle/Documents/AiClub/AI_Club_20-21/small_data"
-CATEG = ["with", "without"]
+CATEG = ["withbig", "withoutBig"]
 
-#Define shape images will be rendered to
+#Define shape 50 x50
 SIZE = 100
-#Format images 
+
 training_data = []
 
-#create training data
+#Normalize data
+#create training data method
 def createTD():
     #iterate through all examples
     for category in CATEG:
@@ -32,11 +29,7 @@ def createTD():
                 training_data.append([new_array, class_num])
             except Exception as e:
                 pass
-"""
-This method changes the values within the training data 
-within between 0-1.
-This is important so the number don't get to big when fitting the model
-"""
+#normalize method      
 def norm(tester):
     new= []
     for i in range(len(tester)):
@@ -49,38 +42,41 @@ def norm(tester):
         new.append(tempx)
     return new
 
+
+#create the training data
 createTD()
-
-#Lines 55-58 make a test set from taking data out of the training data
+print(len(training_data))
 mid = (len(training_data)/2)
-test= []
-for i in range(5,-5,-1):
-    test.append(training_data.pop(int(mid+i)))
 
-    
+#take data out of training data and make test data
+test= []
+for i in range(50,-50,-1):
+    test.append(training_data.pop(int(mid+i)))
 print(len(training_data))
 print(len(test))
 
-#shuffle the list of training data
+#shuffle the list of data
 random.shuffle(training_data)
+random.shuffle(test)
 
 
-"""Seperate the trainging data into 
-(x) the images and (y) the key (mask/noMask)"""
 x=[]
 y=[]
 for features,label in training_data: 
     x.append(features)#the actaul info in the data
     y.append(label) # 1(without mask) or 0 (with mask)
+
 xTest = []
 yTest = []
 for feature,label in test:
     xTest.append(features)
     yTest.append(label)
     
-x = norm(x)    #noramlize data between 0-1
-xTest = norm(xTest)   #noramlize data between 0-1
-input_shape = [SIZE,SIZE,1]   #define the input shape [img_width,img_height,valueRange]
+    
+#noramlize data between 0-1
+x = norm(x)
+xTest = norm(xTest)
+input_shape = [SIZE,SIZE,1]
 
 
 #// build model
@@ -92,21 +88,25 @@ model.add(keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(128, activation='relu'))
 model.add(keras.layers.Dense(1, activation='sigmoid'))
-
 print("compiling model")
+
 # compile model
 opt = Adam(lr=.0001, decay=.0004 / 29)
 model.compile(loss="mse",
              optimizer=opt,
              metrics=['accuracy'])
              
-#Fit the model
-print("fitting model") 
+print("fitting model")       
+#// run data
 x = np.array(x).reshape(len(x),SIZE,SIZE,1).tolist()
 H = model.fit(x,y,batch_size=128, epochs=5)
-
 xTest = np.array(xTest).reshape(len(xTest),SIZE,SIZE,1).tolist()
 print("evaluting")
 loss, acc = model.evaluate(xTest,yTest)  # evaluate the out of sample data with model
-print(loss)  # model's loss (error)
-print(acc)  # model's accuracy
+print("Loss: ",loss)  # model's loss (error)
+print("Accuracy: ", acc)  # model's accuracy
+
+
+
+
+
